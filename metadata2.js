583956913f9fc529c9aded4ed9344e2774e2791c
lilamye2 <amyesocials@gmail.com>
2024-12-15 23:15:34 +0330

    Update metadata2.js

--- metadata2.js
+++ metadata2.js
@@ -1,5 +1,6 @@
 {
   "name": "AMYE",
   "symbol": "AY",
+  "description": "The Token For The First Gambling Platform Based on Smart Contracts and Blockchain;)",
   "uri": "https://raw.githubusercontent.com/lilamye2/logo/refs/heads/main/photo_2024-12-15%2020.57.08.jpeg"
 }
