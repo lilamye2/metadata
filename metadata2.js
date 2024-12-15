b35dc0aa5b494cdd0344d578ab4fdd21ed66d00d
lilamye2 <amyesocials@gmail.com>
2024-12-15 23:14:03 +0330

    Update metadata4.js

--- metadata2.js
+++ metadata2.js
@@ -1,5 +1,6 @@
 {
   "name": "AMYE",
   "symbol": "AY",
+  "description": "Just a test for how to name your token, again and again ;)",
   "image": "https://raw.githubusercontent.com/lilamye2/logo/refs/heads/main/photo_2024-12-15%2020.57.08.jpeg"
 }
